69fc0ab73b8b424224d21c23f9c8f844a7bed911
sleeplesseditor <josh@bryson.me.uk>
2020-07-21 16:47:50 +0100

    Data fetching and initial marker display layer implemented

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,13 +1,41 @@
-import React, { createRef, PureComponent } from 'react';
-import { Map, TileLayer } from 'react-leaflet';
+import React, { createRef, Fragment, PureComponent } from 'react';
+import { readRemoteFile } from 'react-papaparse'
+import { Map, TileLayer, CircleMarker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './map.scss';
+
+  const MarkerLayer = ({ latitude, longitude, ...props }) => (
+    <CircleMarker center={[latitude, longitude]} radius={2}>
+      {/* <Popup>{content}</Popup> */}
+    </CircleMarker>
+  )
+  
+  const MarkersList = ({ markers }) => {
+    const items = markers.map(({ key, ...props }) => (
+      <MarkerLayer key={key} {...props} />
+    ))
+    return <Fragment>{items}</Fragment>
+  }  
 
 export default class MapComponent extends PureComponent {
   state = {
     hasLocation: false,
     lat: 55,
     lng: -2,
+    data: []
+  }
+
+  componentDidMount() {
+    readRemoteFile(
+      'https://s3-us-west-2.amazonaws.com/s.cdpn.io/2814973/charge-points-slim.csv',
+      {
+          header: true,
+          skipEmptyLines: true,
+          complete: (results) => {
+              this.setState({data: results.data});
+          }
+      }
+    )
   }
 
   mapRef = createRef();
@@ -27,6 +55,7 @@ export default class MapComponent extends PureComponent {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+          <MarkersList markers={this.state.data} />
         </Map>
       </div>
     )
